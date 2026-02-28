bcdc15bd80821ff2015e2952535c2a00c78bd288
Hidran Arias <hidran.arias@spryker.com>
2026-02-28 23:45:20 +0100

    Exercise 1: Hello World Back Office - Skeleton

---
+++ src/SprykerAcademy/Zed/HelloWorld/Communication/Controller/HelloController.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * This file is part of the Spryker Commerce OS.
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
+declare(strict_types = 1);
+
+namespace SprykerAcademy\Zed\HelloWorld\Communication\Controller;
+
+use Spryker\Zed\Kernel\Communication\Controller\AbstractController;
+
+class HelloController extends AbstractController
+{
+    public function indexAction(): array
+    {
+        // TODO
+
+        return [];
+    }
+}
