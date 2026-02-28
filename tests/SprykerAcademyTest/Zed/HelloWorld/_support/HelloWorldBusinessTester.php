0d28c330acc464c5a71df23d60fb512da9ee679f
Hidran Arias <hidran.arias@spryker.com>
2026-02-28 23:45:20 +0100

    Exercise 2: Data Transfer Object - Complete

---
+++ tests/SprykerAcademyTest/Zed/HelloWorld/_support/HelloWorldBusinessTester.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SprykerAcademyTest\Zed\HelloWorld;
+
+use Codeception\Actor;
+
+class HelloWorldBusinessTester extends Actor
+{
+    use _generated\HelloWorldBusinessTesterActions;
+}
