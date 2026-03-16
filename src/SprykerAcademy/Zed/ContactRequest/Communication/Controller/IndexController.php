6a02b12eb84a4f33607c801ed24868cf3ac23b18
Hidran Arias <hidran.arias@spryker.com>
2026-03-16 10:00:00 +0100

    ContactRequest back office skeleton

---
+++ src/SprykerAcademy/Zed/ContactRequest/Communication/Controller/IndexController.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * This file is part of the Spryker Commerce OS.
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
+declare(strict_types = 1);
+
+namespace SprykerAcademy\Zed\ContactRequest\Communication\Controller;
+
+use Spryker\Zed\Kernel\Communication\Controller\AbstractController;
+
+class IndexController extends AbstractController
+{
+    public function indexAction(): array
+    {
+        // TODO
+
+        return [];
+    }
+}
